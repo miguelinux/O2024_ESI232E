1b55b154741840955cfd9c4fc519635143259078
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2024-10-31 08:54:21 -0600

    instrucciones de archivos

---
+++ tareas/instrucciones_1.py
@@ -0,0 +1,17 @@
+# Abrir el archivo prueba.txt como escritura no destructiva,
+#   posicionando el puntero al final del archivo
+# Asignar el nombre archivo
+archivo = open("prueba.txt","a")
+
+# Desplegar la posición del puntero
+# >>> aqui falta código
+
+# Añadir 2 líneas de texto
+archivo.write("Una línea nueva\n")
+archivo.write("Otra línea nueva\n")
+
+# Desplegar la nueva posición del puntero
+# >>> aqui falta código
+
+# Cerrar el archivo.
+archivo.close()
